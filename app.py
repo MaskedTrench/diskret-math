757360ecd7397a6c79fb3ebd72013b68acef8e33
MaskedTrench <markushasorokin@yandex.com>
2022-04-15 11:04:00 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template
+from forms import (one_result, two_result, three_result,
+                   fouth_result, five_result, six_result,
+                   double_build_sknf, double_build_sdnf,
+                   third_build_sdnf, third_build_sknf
+                   )
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/first')
+def first():
+    res = one_result()
+    return render_template('result_out.html',
+                           title='one',
+                           result=res,
+                           sknf=double_build_sknf(res),
+                           sdnf=double_build_sdnf(res)
+                          )
+
+
+@app.route('/second')
+def second():
+    res = two_result()
+    return render_template('result_out.html',
+                           title='second',
+                           result=res,
+                           sknf=double_build_sknf(res),
+                           sdnf=double_build_sdnf(res)
+                          )
+
+
+@app.route('/third')
+def third():
+    res = three_result()
+    return render_template('result_out.html',
+                           title='third',
+                           result=res,
+                           sknf=double_build_sknf(res),
+                           sdnf=double_build_sdnf(res)
+                          )
+
+
+@app.route('/fourth')
+def fourth():
+    res = fouth_result()
+    return render_template('result_three.html',
+                           title='fourth',
+                           result=res,
+                           sknf=third_build_sknf(res),
+                           sdnf=third_build_sdnf(res)
+                          )
+
+
+@app.route('/fives')
+def fives():
+    res = five_result()
+    return render_template('result_three.html',
+                           title='fives',
+                           result=res,
+                           sknf=third_build_sknf(res),
+                           sdnf=third_build_sdnf(res)
+                          )
+
+
+@app.route('/sixth')
+def sixth():
+    res = six_result()
+    return render_template('result_three.html',
+                           title='sixth',
+                           result=res,
+                           sknf=third_build_sknf(res),
+                           sdnf=third_build_sdnf(res)
+                          )
+
+if __name__ == '__main__':
+    app.run()
---
+++ forms.py
@@ -0,0 +1,117 @@
+def negative(x): return 0 if x == 1 else 0
+
+
+def implication(x, y): return 1 if x == y else 0
+
+
+def plus(x, y): return 1 if x + y == 2 else x + y
+
+
+def xor(x, y): return 1 if x != y else 0
+
+
+def to(x, y): return 1 if x <= y else 0
+
+
+def double_build_sknf(table):
+    tmp = ''
+    for pair in table:
+        if pair[2] == 0:
+            tmp = f"{tmp}({'-x' if table[0] == 0 else 'x'} v {'-y' if table[1] == 0 else 'y'}) ^ "
+    tmp = tmp[0:len(tmp)-3]
+    return tmp
+
+
+def third_build_sknf(table):
+    tmp = ''
+    for pair in table:
+        if pair[3] == 0:
+            tmp = f"{tmp}({'-x' if table[0] == 0 else 'x'} v {'-y' if table[1] == 0 else 'y'} v {'-z' if table[2] == 0 else 'z'}) ^ "
+    tmp = tmp[0:len(tmp)-3]
+    return tmp
+
+
+def double_build_sdnf(table):
+    tmp = ''
+    for pair in table:
+        if pair[2] == 1:
+            tmp = f"{tmp}({'-x' if table[0] == 0 else 'x'} ^ {'-y' if table[1] == 0 else 'y'}) v "
+    tmp = tmp[0:len(tmp)-3]
+    return tmp
+
+
+def third_build_sdnf(table):
+    tmp = ''
+    for pair in table:
+        if pair[3] == 1:
+            tmp = f"{tmp}({'-x' if table[0] == 0 else 'x'} ^ {'-y' if table[1] == 0 else 'y'} ^ {'-z' if table[2] == 0 else 'z'}) v "
+    tmp = tmp[0:len(tmp)-3]
+    return tmp
+
+
+def one_result():
+    res = list(range(2 ** 2))
+    index = 0
+    for x in [0, 1]:
+        for y in [0, 1]:
+            res[index] = [x, y, plus(x, implication(x, y))]
+            index += 1
+    return res
+
+
+def two_result():
+    res = list(range(2 ** 2))
+    index = 0
+    for x in [0, 1]:
+        for y in [0, 1]:
+            res[index] = [x, y, to(x, negative(y)) * xor(y, negative(x))]
+            index += 1
+    return res
+
+
+def three_result():
+    res = list(range(2 ** 2))
+    index = 0
+    for x in [0, 1]:
+        for y in [0, 1]:
+            res[index] = [x, y, implication(x,
+                                        xor(
+                                            implication(negative(y), y * negative(x)), y
+                                        )
+                                     )
+                          ]
+            index += 1
+    return res
+
+
+def fouth_result():
+    res = list(range(2 ** 3))
+    index = 0
+    for x in [0, 1]:
+        for y in [0, 1]:
+            for z in [0, 1]:
+                res[index] = [x, y, z, xor(x, y * z)]
+                index += 1
+    return res
+
+
+def five_result():
+    res = list(range(2 ** 3))
+    index = 0
+    for x in [0, 1]:
+        for y in [0, 1]:
+            for z in [0, 1]:
+                res[index] = [x, y, z, implication(plus(x, y), negative(x) * z)]
+                index += 1
+    return res
+
+
+def six_result():
+    res = list(range(2 ** 3))
+    index = 0
+    for x in [0, 1]:
+        for y in [0, 1]:
+            for z in [0, 1]:
+                res[index] = [x, y, z, plus(negative(x), to(implication(xor(z, y), x), negative(z)))]
+                index += 1
+    return res
